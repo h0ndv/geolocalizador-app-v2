a9685e7b14909c3ea8a91ddaba5ef5cb01b1d7c7
h0ndv <118297178+h0ndv@users.noreply.github.com>
2025-05-22 23:05:46 -0400

    feat: Añadir layout del dashboard

--- app/src/main/java/com/example/geolocalizador_app_v2/Dashboard.kt
+++ app/src/main/java/com/example/geolocalizador_app_v2/Dashboard.kt
@@ -5,23 +5,16 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import android.content.Intent
-import android.widget.Button
 
-class MainActivity : AppCompatActivity() {
+class Dashboard : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        setContentView(R.layout.activity_dashboard)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.dashboard)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
-
-        // Abrir dashboard al iniciar sesion
-        findViewById<Button>(R.id.btnIniciarSesion).setOnClickListener {
-            startActivity(Intent(this, Dashboard::class.java))
         }
     }
 }
